b8e408ebce19b176263347873f50713e66382e53
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-29 10:23:13 +0530

    fix(app): import all components and error fixes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import { Route, Switch, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import NewPost from "./components/NewPost";
+import NotFound from "./components/NotFound";
+import PostPage from "./components/PostPage";
+
 
 
 function App() {
