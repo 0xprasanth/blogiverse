c97bc20d266778b1ab6127acb8e009c4a07f7350
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-30 23:33:51 +0530

    fix(data): add new data such as username and data&time

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,34 +1,37 @@
 export const postData = [
   {
-    userId: 1,
+    user: "Mark Antony",
+    date: "Augest 12, 2023 12:45 PM",
     id: 1,
-    title:
-      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    title: "MY first Post",
     body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
   },
   {
-    userId: 1,
+    user: "Basha Bhai",
+    date: "December 12, 2022 00:45 AM",
     id: 2,
     title: "qui est esse",
     body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
   },
   {
-    userId: 1,
+    user: "Kaanchana",
+    date: "Feburary 14, 2023 11:05 PM",
     id: 3,
-    title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    title: "Not Too Big Title here",
     body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
   },
   {
-    userId: 1,
+    user: "A2D Soldier",
+    date: "November 23, 1989 01:45 PM",
     id: 4,
     title: "eum et est occaecati",
     body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
   },
   {
-    userId: 1,
+    user: "Gojo Sautoro",
+    date: "May 22, 2020 06:27 PM",
     id: 5,
     title: "nesciunt quas odio",
     body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
   },
 ];
-
