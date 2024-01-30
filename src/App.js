695a486be87b6469d1cd811bb6f2896f37c4d42b
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-30 18:50:19 +0530

    feat(core)!!: routes are stable now
    BREAKING CHANGE: The use of Routes and Route are identify and with help element props instead of component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import About from "./components/About";
 import Footer from "./components/Footer";
@@ -8,20 +7,23 @@ import Nav from "./components/Nav";
 import NewPost from "./components/NewPost";
 import NotFound from "./components/NotFound";
 import PostPage from "./components/PostPage";
-
-
+// router-dom import
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
-      <Home />
-      <NewPost />
-      <PostPage />
-      <About />
-      <Footer  />
-
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/post" element={<NewPost />}/>
+        <Route path="/post/:id" element={<PostPage />}/>
+        <Route path="/about" element={<About />}/>
+        {/* not found */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
