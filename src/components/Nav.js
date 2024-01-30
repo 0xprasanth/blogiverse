ebe85ecc7fc4cac94bcb23db25795c957db847e3
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-30 19:23:39 +0530

    feat(app): add Nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import Header from './Header';
+import { Link } from 'react-router-dom';
 
-const Nav = () => {
+const Nav = ({search, setSearch}) => {
   return (
-    <nav>
-        <h1>Nav</h1>
+    <>
+    <nav className='Nav'>
+      <Header title={'blogiverse'}  />
+      <form className='searchForm' onSubmit={e => e.preventDefault()}>
+        <label htmlFor="search">
+          Search Post
+        </label>
+        {/* Controlled Form input */}
+        <input 
+          id='search'
+          type='text'
+          placeholder='Search for Posts'
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          />
+
+      </form>
+      <ul>
+        <li>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/post'}>Post</Link>
+          <Link to={'/about'}>About</Link>
+        </li>
+      </ul>
     </nav>
+    
+    </>
     
   )
 }
