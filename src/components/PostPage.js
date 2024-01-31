dd82db91439d2df70055549dad09eb33606359e7
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-31 10:19:44 +0530

    feat(app): create Post Page that displays the posts given the :id as params

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useParams, Link} from 'react-router-dom'
 
-const PostPage = () => {
+const PostPage = ({ posts, handleDelete}) => {
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id)
   return (
-    <main>
-        <h1>PostPage</h1>
+    <main className='PostPage'>
+      <article className='post'>
+        {
+          post && 
+            <>
+              <h2>{post.title}</h2> 
+              <p className="postData">{post.user + " • " + post.date}</p>
+              <p className="postBody">{post.body}</p>
+              <button className='deleteBtn' onClick={()=> handleDelete(post.id)}> Delete</button>
+            </>
+        }
+        </article>
+        {
+          // post is not there
+          !posts &&
+            <>
+              <h2>Post not found</h2>
+              <p>
+                <Link to='/'>Vist Home Page</Link>
+              </p>  
+            </>
+        }
     </main>
   )
 }
