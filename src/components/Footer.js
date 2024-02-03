4514c178b473b6c06f2de813547d64fa4648a42b
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-03 10:56:10 +0530

    fix(app): add github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import logo from '../asset/github-mark.svg'
 
 const Footer = ({ postsCount }) => {
+  const logoStyles = {
+    width: '15%',
+    // height: '1rem'
+
+  }
   return (
     <footer>
-        <h3>
+      <div className="footer">
+
+        <h2>
           {postsCount} Posts 
-        </h3>
+        </h2>
+        <a href="http://github.com/ptech12/blogiverse" target="_blank" rel="noopener noreferrer"
+        >
+          <img src={logo} style={logoStyles}/>
+        </a>
+      </div>
     </footer>
   )
 }
