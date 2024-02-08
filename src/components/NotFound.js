8fd105e63ac321ac6b6a15294e1ab6a1dcbd936b
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-08 23:19:41 +0530

    fix(app): notfound component finish

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import dottedFace from '../asset/dotted-face.svg'
 
 const NotFound = () => {
   return (
-    <main>
-        <h1>Not Found</h1>
+    <main className='NotFound'>
+      <img width={'20%'} className='sad-face' src={dottedFace} alt="dotted-face-emoji" />
+        <h2> 404 Not Found</h2>
+        <h4>oops, something went wrong</h4>
+        <Link to={"/"}>Visit Homepage</Link>
     </main>
   )
 }
