98e50697549e7b51a2f8366e48cfbb72876e1c84
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-15 23:51:17 +0530

    fix(core): add DataContext provider in which the components use the data

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,12 +1,11 @@
 import { createContext, useState, useEffect } from "react";
 // // router-dom import
 // import { useNavigate } from "react-router-dom";
-// import { format } from "date-fns";
+import { format } from "date-fns";
 // // api routes
-// import api from "../api/posts";
-// import userApi from "../api/users";
-// import useWindowSize from "../hooks/useWindowSize";
-
+import api from "../api/posts";
+import userApi from "../api/users";
+import useWindowSize from "../hooks/useWindowSize";
 import useAxiosFetch from '../hooks/useAxiosFetch';
 
 const DataContext = createContext({});
@@ -15,6 +14,8 @@ export const DataProvider = ({ children }) => {
     const [posts, setPosts] = useState([])
     const [search, setSearch] = useState('');
     const [searchResults, setSearchResults] = useState([]);
+
+    const [users, setUsers] = useState([]);
 
     const { data, fetchError, isLoading } = useAxiosFetch('https://dummyjson.com/posts?limit=30');
 
@@ -29,12 +30,13 @@ export const DataProvider = ({ children }) => {
 
         setSearchResults(filteredResults.reverse());
     }, [posts, search])
+    const { width } = useWindowSize();
 
     return (
         <DataContext.Provider value={{
             search, setSearch,
             searchResults, fetchError, isLoading,
-            posts, setPosts
+            posts, setPosts, width
         }}>
             {children}
         </DataContext.Provider>
