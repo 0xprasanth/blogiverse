0a76958106d808509cba5a4fceb24dc2ecaa5b5c
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-16 00:41:43 +0530

    fix(data-provider): remove unused function and clean up around it

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,10 +1,7 @@
 import { createContext, useState, useEffect } from "react";
 // // router-dom import
 import { useNavigate } from "react-router-dom";
-import { format } from "date-fns";
-// // api routes
-import api from "../api/posts";
-import userApi from "../api/users";
+
 import useWindowSize from "../hooks/useWindowSize";
 import useAxiosFetch from "../hooks/useAxiosFetch";
 
@@ -14,95 +11,35 @@ export const DataProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  // new post state
-  const [postTitle, setPostTitle] = useState("");
-  const [postBody, setPostBody] = useState("");
-  const [editTitle, setEditTitle] = useState("");
-  const [editBody, setEditBody] = useState("");
-  const [users, setUsers] = useState([]);
 
 
   const { data, fetchError, isLoading } = useAxiosFetch(
     "https://dummyjson.com/posts?limit=30"
   );
 
-  const navigate = useNavigate();
+  /*
+    posts X search are dependency 
+    as our search input provides new data
+    to the [search's] essentially filter the posts
+    that both matches [search] 
+  */
+    useEffect(() => {
+        // console.log(posts);
+        const filteredResults = posts.filter(
+          (post) =>
+            post.body.toLowerCase().includes(search.toLowerCase()) || // OR short circuit
+            post.title.toLowerCase().includes(search.toLowerCase())
+        );
+        setSearchResults(filteredResults);
+      }, [posts, search]);
+    
+    
   useEffect(() => {
     setPosts(data);
   }, [data]);
 
-  useEffect(() => {
-    const filteredResults = posts.filter(
-      (post) =>
-        post.body.toLowerCase().includes(search.toLowerCase()) ||
-        post.title.toLowerCase().includes(search.toLowerCase())
-    );
 
-    setSearchResults(filteredResults.reverse());
-  }, [posts, search]);
   const { width } = useWindowSize();
-
-  /* handle submit func to submit new post */
-  const handleSubmit = async (e) => {
-    // C in Create
-    e.preventDefault();
-    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-    // const datetime = format(new Date(), 'MMM dd, yyyy p');
-    const newPost = { id, title: postTitle, body: postBody };
-
-    try {
-      const resp = await api.post("/posts/add", newPost);
-      const allPosts = [...posts, resp.data];
-      setPosts(allPosts);
-      setPostTitle("");
-      setPostBody("");
-      navigate("/");
-    } catch (err) {
-      console.error(`Error: ${err.message}`);
-      alert(`Error: ${err.message}`);
-    }
-  };
-
-  const handleEdit = async (id) => {
-    const datetime = format(new Date(), "MMM dd, yyyy p");
-    const updatePost = { id, title: editTitle, datetime, body: editBody };
-    try {
-      // update block
-      const resp = await api.put(`/posts?id=${id}`, updatePost);
-      // only update the posts that match the id else keep it as it is
-      setPosts(posts.map((post) => (post.id === id ? { ...resp.data } : post)));
-      setEditBody("");
-      setEditTitle("");
-      navigate("/");
-    } catch (err) {
-      console.error(`Error: ${err.message}`);
-    }
-  };
-
-  /* habndle delete */
-  const handleDelete = async (id) => {
-    // D for delete
-    try {
-      const postList = posts.filter((post) => post.id !== id);
-      setPosts(postList);
-      navigate("/");
-    } catch (err) {
-      console.error(`Error: ${err.message}`);
-    }
-  };
-  /* Belongs to PostPage component */
-  const displayTag = (tags) => {
-    return (
-      tags.map(newTag => {
-        return newTag
-      })
-    )
-  }
-
-  const displayUserName = (u) => {
-    return u.firstName +' '+ u.lastName
-  }
-
 
   return (
     <DataContext.Provider
@@ -115,19 +52,15 @@ export const DataProvider = ({ children }) => {
         posts,
         setPosts,
         width,
-        handleSubmit,
-        handleDelete,
-        handleEdit,
-        postTitle,
-        setPostTitle,
-        postBody,
-        setPostBody,
-        editTitle,
-        setEditTitle,
-        editBody,
-        setEditBody,
-        displayTag,
-        displayUserName,
+        // handleSubmit,
+        // handleDelete,
+        // handleEdit,
+        // editTitle,
+        // setEditTitle,
+        // editBody,
+        // setEditBody,
+        // displayTag,
+        // displayUserName,
       }}
     >
       {children}
